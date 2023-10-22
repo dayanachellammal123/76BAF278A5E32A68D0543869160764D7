65c9cdc98301c7d512ef3bf787fb0c16b1de412e
dayanachellammal123 <148514876+dayanachellammal123@users.noreply.github.com>
2023-10-22 10:22:15 +0530

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,23 @@
+mmClass BankAccount:
+    def__(selfaccount_number,account_holder_name,initial_balance=0.0):
+self_account_number=account_number 
+self_account_holder=name=account_holder _name 
+self_account_balance=initial_balance
+def.deposite(self, amount):
+if amount >0:
+self_account_balance+=amount
+print ("Deposited a^,1{}.new balance:a^,1{}".format(amount,self_account_balance)
+       else:
+print ("Invalid deposit amount.")
+def with draw(self, amount ):
+if amount>0 and amount<=self_account_
+balance:
+  self_account_balance_=amount
+print ("Withdraw a^,1{}.new balance:a^1{}".format(amount,self_account_balance))
+else:
+print ("Invalid withdrawal amount or insufficient balance.")
+def.displ
+
+
+       
+        
